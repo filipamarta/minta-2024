9fd86693a478a6bd1c9d8180f924597f97015734
Filipa Marta <filipa.marta@holisticon.de>
2024-09-01 19:11:59 +0100

    menu

---
+++ home.js
@@ -0,0 +1,10 @@
+import { animate } from "motion";
+
+animate(
+  ".cantaloupe",
+  {
+    y: [-100, 0],
+    opacity: [0, 1],
+  },
+  { duration: 1, delay: 0.5 }
+);
--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 import "./style.css";
 import { animate, inView } from "motion";
 
+let notMobile = window.matchMedia("(min-width: 768px)");
+
+const menuHeader = document.querySelector("header");
+const menuNav = document.querySelector("header nav");
+const menuNavLogo = document.querySelector("header nav .logo");
+const menuLinkElements = document.querySelector("header nav ul");
+const menuButton = document.querySelector("button.hamburger");
+const menuIconClose = document.querySelector(".menu-close");
+const menuIconOpen = document.querySelector(".menu-open");
+
 animate(
   "header",
   {
@@ -8,15 +18,6 @@ animate(
     opacity: [0, 1],
   },
   { duration: 1, delay: 0 }
-);
-
-animate(
-  ".cantaloupe",
-  {
-    y: [-100, 0],
-    opacity: [0, 1],
-  },
-  { duration: 1, delay: 0.5 }
 );
 
 animate(
@@ -33,3 +34,46 @@ inView("section.content", (info) => {
     { duration: 1, delay: 0.5 }
   );
 });
+
+/* TOGGLE MENU */
+const toggleMenu = () => {
+  if (menuIconClose.style.display === "none") {
+    menuIconOpen.style.display = "none";
+    menuIconClose.style.display = "block";
+    menuLinkElements.style.display = "block";
+    menuHeader.classList.add("show");
+  } else {
+    menuNavLogo.style.display = "block";
+    menuIconOpen.style.display = "block";
+    menuIconClose.style.display = "none";
+    menuLinkElements.style.display = "none";
+    menuHeader.classList.remove("show");
+  }
+};
+
+if (notMobile.matches) {
+  menuButton.remove();
+} else {
+  menuNav.appendChild(menuButton);
+  menuIconClose.style.display = "none";
+}
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth >= 768) {
+    menuLinkElements.style.display = "flex";
+    menuButton.style.display = "none";
+  } else {
+    menuButton.style.display = "block";
+    menuIconOpen.style.display = "block";
+    menuLinkElements.style.display = "none";
+    menuIconClose.style.display = "none";
+  }
+});
+
+menuButton.addEventListener(
+  "click",
+  () => {
+    toggleMenu();
+  },
+  true
+);
