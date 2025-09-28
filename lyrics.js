db85b3cb3f1c187666910a8ad09fcb72c4d01733
Filipa Marta <filipa.marta@gmail.com>
2025-09-28 11:48:10 +0100

    lyrics animation

---
+++ lyrics.js
@@ -0,0 +1,25 @@
+import { animate, inView } from "motion";
+
+animate(
+  ".lyrics-intro",
+  {
+    y: [-100, 0],
+    opacity: [0, 1],
+  },
+  { duration: 0.4, delay: 0 }
+);
+
+animate(
+  "section.lyrics-wrapper h2, section.lyrics-wrapper ul, section.lyrics-wrapper img",
+  {
+    opacity: 0,
+  }
+);
+
+inView("section.lyrics-wrapper", (info) => {
+  animate(
+    info.target.querySelectorAll("h2, ul, img"),
+    { opacity: 1 },
+    { duration: 1, delay: 0.5 }
+  );
+});
